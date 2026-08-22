803d4a7994c6e59bd921220382a2a462fc0e4327
Riya Mehta <riyawork2006@gmail.com>
2026-08-22 17:38:53 +0530

    error in login and running on localhost not dockerized db

--- backend/database/dayflow/settings.py
+++ backend/database/dayflow/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*%o0xlhbiy@op8@k3y*#**shk41js#922&84!jx&7d8xk_1e6%'
+SECRET_KEY = SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
